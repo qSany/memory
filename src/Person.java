8aa9d08fa5132f574a5aea9ff625384570fa36b5
admin-rez <kraken_1111@mail.ru>
2023-12-05 09:39:15 +0300

    memory

---
+++ src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    private String name;
+    private String surname;
+    public void changePerson(Person person)
+        person =new Person();
+        person.name = "Ilya";
+     person.surname = "Lagutenko";
+}
---
+++ src/memory.java
@@ -0,0 +1,37 @@
+public class memory {
+    public static void main(String[] args) {
+        int value = 33;
+        changeValue(value);
+        System.out.println(value);
+        System.out.println("-----------------");
+        Integer value2 = 33;
+        changeValue(value2);
+        System.out.println(value2);
+        System.out.println("-----------------");
+        Integer[] array = new Integer[]{3, 4};
+        changeValue(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println("-----------------");
+        changeArrayValue(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println("-----------------");
+
+    }
+
+    private static void changeValue(int value) {
+        value = 22;
+    }
+
+    private static void changeValue(Integer value) {
+        value = 22;
+    }
+
+    private static void changeValue(Integer[] arr) {
+        arr = new Integer[]{1, 2};
+    }
+
+    private static void changeArrayValue(Integer[] arr) {
+        arr[0] = 99;
+    }
+}
+
